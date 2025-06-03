fba5707d0cf54e179a3950ea912573a2f523bec0
restuandrie <restuandrie@gmail.com>
2025-06-03 22:00:12 +0700

    Delete public/icons directory

--- public/icons/components/AddBuyerModal.tsx
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left blank to signify its deletion.
-// The AddBuyerModal component is no longer needed as sellers will
-// select buyers from a list of existing users, and buyers will register themselves.
--- public/icons/components/UserSwitcher.tsx
+++
@@ -1,2 +0,0 @@
-// This file is no longer needed and can be deleted.
-// Kept empty to signify deletion in the diff.
